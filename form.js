741c7291074e06c5ca828f9440fe992b084d0ae0
Cesar Rosario <cesar.c.rosario@gmail.com>
2024-06-11 23:30:55 -0400

    progress commit

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
 
 let faction = document.getElementById("factionChoice");
+let factionBG = document.getElementById("registrationForm");
 let chosenFaction = faction.value;
 
 const warlordLists = Array.from(document.querySelectorAll('[id*="Input"]'), (list) => {
@@ -9,10 +10,49 @@ const warlordLists = Array.from(document.querySelectorAll('[id*="Input"]'), (lis
     }
 });
 
+let registeredPlayers = [
+    {
+        playerName:"Jacob Soucy",
+        faction:"Nords",
+        warlord:"Konungyr",
+        listPoints:"1990",
+        playerPin:"4FK732"
+
+    },
+
+    {
+        playerName:"Aleksandar Favrro",
+        faction:"Old Dominion",
+        warlord:"Fallen Divinity",
+        listPoints:"2000",
+        playerPin:"1OD0JJ"
+
+    },
+
+    {
+        playerName:"Magdalena Mohammad",
+        faction:"Spires",
+        warlord:"Lineage Prideborne",
+        listPoints:"2000",
+        playerPin:"H873KJ"
+
+    },
+
+    {
+        playerName:"Rue Anker",
+        faction:"Hundred Kingdoms",
+        warlord:"Priory Commander (Crimson Tower)",
+        listPoints:"1985",
+        playerPin:"I37NC8"
+
+    }
+];
+
 
 faction.addEventListener("change", () => {
     chosenFaction = faction.value;
     document.getElementById("factionPlaceholder").style.display = 'none';
+    factionBG.style.backgroundPosition = 'center';
 
     if (warlordLists){
         let i=0
@@ -30,34 +70,46 @@ faction.addEventListener("change", () => {
 
         case 'City States':
             document.getElementById("warlordCSInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/Citystate-Massbattle-Illus_web-1.jpg\')';
+            factionBG.style.backgroundPosition = 'center bottom';            
             break;
         
         case 'Dweghom':
             document.getElementById("warlordDweghomInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/Fall_of_Dragons.jpg\')';
+            factionBG.style.backgroundPosition = 'center bottom';
             break;
         
         case 'Hundred Kingdoms':
             document.getElementById("warlordHundredKingdomsInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/Huskarls-vs-Crimson-Tower.jpg\')';
+
             break;
 
         case 'Nords':
             document.getElementById("warlordNordsInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/Giant-vs-worm.jpg\')';
             break;
 
         case 'Old Dominion':
-        document.getElementById("warlordOldDominionInput").style.display = 'inline-block';
+            document.getElementById("warlordOldDominionInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/The-Legions.jpg\')';
             break;
 
         case 'Sorcerer Kings':
             document.getElementById("warlordSorcererKingsInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/sorcerer-kings-e.jpg\')';
             break;
 
         case 'Spires':
             document.getElementById("warlordSpiresInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/NEPENTHE-_The_Battle_q8r2-li.jpg\')';
+            factionBG.style.backgroundPosition = 'center top';
             break;
 
         case 'W\'adrhun':
             document.getElementById("warlordWadruhnInput").style.display = 'inline-block';
+            factionBG.style.backgroundImage = 'linear-gradient(45deg,rgba(255, 255, 255, 0.75),rgba(0, 0, 0, 0.75)),url(\'assets/imgs/The-Wadrhun.jpg\')';
             break;
     }    
 });
